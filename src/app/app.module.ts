b0cc2b850727709a925a278dac9f10b916a330bc
Johnny Meza <xlpacman805@gmail.com>
2020-04-25 21:42:44 -0700

    Created launch-list componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LaunchDetailsComponent } from './launch-details/launch-details.component';
+import { LaunchListComponent } from './launch-list/launch-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LaunchDetailsComponent
+    LaunchDetailsComponent,
+    LaunchListComponent
   ],
   imports: [
     BrowserModule,
